217845b4e776712c53f801e46e2d1cf57f6853b5
generous-vector-9581 <ojappah@gmail.com>
2019-09-24 23:43:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,6 +4,10 @@ end
 def sort_array_desc (array)
   array.sort {|x,y| y <=> x }
 end
+def sort_array_char_count(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
 def swap_elements (array)
   array[1], array[2] = array[2], array[1]
   array
