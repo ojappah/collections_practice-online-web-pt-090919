7ad1d3bb64373ca5163d1e81cff846bae918683d
generous-vector-9581 <ojappah@gmail.com>
2019-09-24 23:48:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,7 +5,7 @@ def sort_array_desc (array)
   array.sort {|x,y| y <=> x }
 end
 def sort_array_char_count(array)
-  array.sort {|x| x.length}
+  array.sort.length {|x|}
   array
 end
 def swap_elements (array)
